8731d4028a6c2f74746a927cb553d4378e972c69
seoh77 <seoh7dev@gmail.com>
2025-10-26 22:42:12 +0900

    입문 #7-2. 순수 JDBC

--- 입문편/hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
+++ 입문편/hello-spring/src/main/java/hello/hello_spring/SpringConfig.java
@@ -1,14 +1,25 @@
 package hello.hello_spring;
 
+import hello.hello_spring.repository.JdbcMemberRepository;
 import hello.hello_spring.repository.MemberRepository;
 import hello.hello_spring.repository.MemoryMemberRepository;
 import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 // 자바 코드로 직접 스프링 빈 등록하기
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -17,6 +28,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
