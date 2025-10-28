5d93d0b71888f3289c20e3ef6884656ba09553a9
seoh77 <seoh7dev@gmail.com>
2025-10-28 10:20:42 +0900

    기본 #4-4. AppConfig 리팩터링

--- 기본편/core/src/main/java/hello/core/AppConfig.java
+++ 기본편/core/src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -12,10 +14,19 @@ public class AppConfig {
 
     // 생성자를 통해서 주입(연결)
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    // `new MemoryMemberRepository()` 중복이 제거 -> 다른 구현체로 변경할 때 한 부분만 변경하면 됨.
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
