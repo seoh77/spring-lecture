f1376184617aa01af6bcf9c0b9c2c6a61c17dfcb
seoh77 <seoh7dev@gmail.com>
2025-10-28 11:08:24 +0900

    기본 #4-9. 스프링으로 전환하기

--- 기본편/core/src/main/java/hello/core/AppConfig.java
+++ 기본편/core/src/main/java/hello/core/AppConfig.java
@@ -9,24 +9,29 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // AppConfig는 애플리케이션의 실제 동작에 필요한 구현 객체를 생성한다.
+@Configuration  // 기존에는 개발자가 AppConfig를 사용해서 직접 객체를 생성하고 DI를 했지만, 이제부터는 스프링 컨테이너를 통해 사용
 public class AppConfig {
 
-    // 생성자를 통해서 주입(연결)
+    @Bean   // 스프링 빈은 `@Bean`이 붙은 메서드의 명을 스프링 빈의 이름으로 사용
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    // `new MemoryMemberRepository()` 중복이 제거 -> 다른 구현체로 변경할 때 한 부분만 변경하면 됨.
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
