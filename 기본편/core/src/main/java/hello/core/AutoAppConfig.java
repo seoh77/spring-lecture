0f017f91439c1dfac46efbabb2a1c12856284263
seoh77 <seoh7dev@gmail.com>
2025-10-28 23:10:51 +0900

    기본 #7-2. 탐색 위치와 기본 스캔 대상

--- 기본편/core/src/main/java/hello/core/AutoAppConfig.java
+++ 기본편/core/src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(     // `@ComponentScan`은 `@Component`가 붙은 모든 클래스를 스프링 빈으로 등록
+        basePackages = "hello.core",    // 컴포넌트 스캔 시작 위치를 지정 -> 만약 지정하지 않으면 `@ComponentScan`이 붙은 설정 정보 클래스의 패키지가 시작 위치가 된다.
         /*
             컴포넌트 스캔을 사용하면 @Configuration이 붙은 설정 정보도 자동으로 등록되기 때문에,
             AppConfig, TestConfig 등 앞서 만들어두었던 설정 정보도 함께 등록되고, 실행되어 버린다.
--- 기본편/core/src/main/java/hello/core/CoreApplication.java
+++ 기본편/core/src/main/java/hello/core/CoreApplication.java
@@ -3,6 +3,10 @@ package hello.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*
+	스프링 부트를 사용하면 스프링 부트의 대표 시작 정보인 @SpringBootApplication를 이 프로젝트 시작 루트 위치에 두는 것이 관례이다.
+	그리고 이 설정 안에 @ComponentScan이 들어있다.
+ */
 @SpringBootApplication
 public class CoreApplication {
 
