79cd4e2b57e23229033651cc92b15ab5f1902b2f
seoh77 <seoh7dev@gmail.com>
2025-10-29 16:14:00 +0900

    기본 #8-3. 생성자 주입을 선택해라

--- 기본편/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 기본편/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,9 +9,11 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
+    // 생성자 주입 방식만 `final` 키워드를 사용할 수 있다.
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    // 기본으로 생성자 주입을 사용하고, 필수 값이 아닌 경우에는 수정자 주입 방식을 옵션으로 부여하면 된다.
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
