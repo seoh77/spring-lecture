a9c27fdc09b3cefc838d637a98724639b9bd38e5
seoh77 <seoh7dev@gmail.com>
2025-10-29 17:23:39 +0900

    기본 #8-6. @Autowired 필드 명, @Qualifier, @Primary

--- 기본편/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ 기본편/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,9 +2,13 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+//@Qualifier("mainDiscountPolicy")  // @Qualifier와 @Primary를 같이 사용하면 @Qualifier가 더 우선권이 높다.
+@Primary    // 우선순위를 정하는 방법 -> @Autowired 시에 여러 빈이 매칭되면 @Primary가 우선권을 가진다.
 public class RateDiscountPolicy implements DiscountPolicy {
 
     private int discountRercent = 10;   // 10% 할인
--- 기본편/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 기본편/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,6 +5,8 @@ import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,6 +16,31 @@ public class OrderServiceImpl implements OrderService {
     // 생성자 주입 방식만 `final` 키워드를 사용할 수 있다.
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    /*
+        @Autowired 매칭 정리
+        1. 타입 매칭
+        2. 타입 매칭의 결과가 2개 이상일 때 필드명, 파라미터명으로 빈 이름 매칭
+
+        @Autowired
+        public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) {
+            this.memberRepository = memberRepository;
+            this.discountPolicy = rateDiscountPolicy;
+        }
+     */
+
+    /* 주입시에 @Qualifier를 붙여주고 등록한 이름을 적어준다.
+
+        @Qualifier 매칭 정리
+        1. @Qualifier 끼리 매칭
+        2. 빈 이름 매칭
+        3. `NoSuchBeanDefinitionException` 예외 발생
+
+        public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+            this.memberRepository = memberRepository;
+            this.discountPolicy = discountPolicy;
+        }
+     */
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
