ad27bfd35291135a7a4983d0ca8e090c4561ad86
seoh77 <seoh7dev@gmail.com>
2025-11-12 18:21:57 +0900

    MVC1 #4-6. MVC 패턴 - 한계

--- MVC1/servlet/src/main/java/hello/servlet/web/servletmvc/MvcMemberListServlet.java
+++ MVC1/servlet/src/main/java/hello/servlet/web/servletmvc/MvcMemberListServlet.java
@@ -12,6 +12,19 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
+/*
+    MVC 패턴 한계
+    - 포워드 중복
+      : View로 이동하는 코드가 항상 중복 호출되어야 한다. 이 부분을 메서드로 공통화해도 되지만, 메서드도 항상 직접 호출해야 한다.
+    - ViewPath 중복
+    - 사용하지 않는 코드
+      : `HttpServletRequest request, HttpServletResponse response` 코드는 사용할 때도 있고, 사용하지 않을 때도 있다.
+    - 공통 처리가 어렵다.
+
+    → 이 문제를 해결하려면 컨트롤러 호출 전에 먼저 공통 기능을 처리해야 한다.
+      프론트 컨트롤러(Front Controller) 패턴을 도입하면 이런 문제를 깔끔하게 해결할 수 있다. (스프링 MVC의 핵심도 이 프론트 컨트롤러이다)
+ */
+
 @WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
 public class MvcMemberListServlet extends HttpServlet {
 
