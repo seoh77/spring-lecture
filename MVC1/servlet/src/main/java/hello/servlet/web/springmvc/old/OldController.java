e877d8ed0096aeafd715939df6eb58e76cadf4cd
seoh77 <seoh7dev@gmail.com>
2025-11-13 22:54:16 +0900

    MVC1 #6-2. 핸들러 매핑과 핸들러 어댑터

---
+++ MVC1/servlet/src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -0,0 +1,29 @@
+package hello.servlet.web.springmvc.old;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+/*
+    이 컨트롤러가 호출되려면 2가지가 필요하다.
+    1. HandlerMapping (핸들러 매핑)
+       - 핸들러 매핑에서 이 컨트롤러를 찾을 수 있어야 한다.
+       - 예) 스프링 빈의 이름으로 핸들러를 찾을 수 있는 핸들러 매핑이 필요하다.
+    2. HandlerAdapter (핸들러 어댑터)
+       - 핸들러 매핑을 통해서 찾은 핸들러를 실행할 수 있는 핸들러 어댑터가 필요하다.
+       - 예) Controller 인터페이스를 실행할 수 있는 핸들러 어댑터를 찾고 실행해야 한다.
+
+    → 스프링은 이미 필요한 핸들러 매핑과 핸들러 어댑터를 대부분 구현해두었다. 개발자가 직접 핸들러 매핑과 핸들러 어댑터를 만드는 일은 거의 없다.
+ */
+
+@Component("/springmvc/old-controller")
+public class OldController implements Controller {
+
+    @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("OldController.handleRequest");
+        return null;
+    }
+}
