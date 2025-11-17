bc799ce7776f7ed24829e115475dec38258fb83a
seoh77 <seoh7dev@gmail.com>
2025-11-17 22:06:11 +0900

    MVC1 #8-5. 상품 목록 - 타임리프

---
+++ MVC1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -0,0 +1,37 @@
+package hello.itemservice.web.basic;
+
+import hello.itemservice.domain.item.Item;
+import hello.itemservice.domain.item.ItemRepository;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/basic/items")
+@RequiredArgsConstructor        // `final`이 붙은 멤버변수만 사용해서 생성자를 자동으로 만들어준다.
+public class BasicItemController {
+
+    private final ItemRepository itemRepository;
+
+    @GetMapping
+    public String item(Model model) {
+        List<Item> items = itemRepository.findAll();
+        model.addAttribute("items", items);
+        return "basic/items";
+    }
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct      // 해당 빈의 의존관계가 모두 주입되고 나면 초기화 용도로 호출된다.
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+}
