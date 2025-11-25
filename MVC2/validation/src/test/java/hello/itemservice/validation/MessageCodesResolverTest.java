dc0c0a8c9d83105fd6fb11b53c46537dea379eff
seoh77 <seoh7dev@gmail.com>
2025-11-25 20:50:48 +0900

    MVC2 #5-12. 오류 코드와 메시지 처리4

---
+++ MVC2/validation/src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -0,0 +1,36 @@
+package hello.itemservice.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MessageCodesResolverTest {
+
+    /*
+        MessageCodesResolver
+        - 검증 오류 코드로 메시지 코드를 생성한다.
+        - `MessageCodesResolver` 인터페이스이고, `DefaultMessageCodesResolver`는 기본 구현체이다.
+        - 주로 `ObjectError`, `FieldError`와 함께 사용
+     */
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @Test
+    void messageCodesResolverObject() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
+        assertThat(messageCodes).containsExactly("required.item", "required");
+    }
+
+    @Test
+    void messageCodesResolverField() {
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+        assertThat(messageCodes).containsExactly(
+                "required.item.itemName",
+                "required.itemName",
+                "required.java.lang.String",
+                "required"
+        );
+    }
+}
